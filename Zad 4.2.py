c665f7679ffd7d969f4423b92d9c083d4f06887a
Jan Szyprzak <jan.szyprzak@gmail.com>
2022-11-25 15:32:28 +0100

    Add description

--- Zad 4.2.py
+++ Zad 4.2.py
@@ -1,12 +1,6 @@
 
-
 def palindrom(word):
-    for i in range(len(word)):
-        if word[i] == word[-1 - i]:
-            return True
-        else:
-            return False
-
-
-
-print(palindrom("test"))
+    """Checks if word is a palindrom,
+        Arguments:
+        word     
+    """
