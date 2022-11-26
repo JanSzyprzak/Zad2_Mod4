9efc88290141334cd66b15995161dc6bc32ee031
Jan Szyprzak <jan.szyprzak@gmail.com>
2022-11-26 12:18:01 +0100

    Add body of the function

--- Zad 4.2.py
+++ Zad 4.2.py
@@ -1,6 +1,17 @@
 
+
 def palindrom(word):
-    """Checks if word is a palindrom,
+    """ 
+        Checks if word is a palindrom,
         Arguments:
         word     
     """
+    for i in range(len(word)):       
+        if word[i] == word[- i - 1] and i + 1 < len(word):           
+            continue
+        elif word[i] == word[- i - 1] and i + 1 == len(word):                    
+            return True
+        else:                      
+            return False
+
+print(palindrom("kajak"))
